ef15ae9d722c02c4ee6992ac3b6806cec5da1214
mateuszwosik <mateusz.wosik@gmail.com>
2016-11-27 17:01:06 +0100

    done

--- app/src/main/java/mw/layoutexample/androidlayoutexample/MainActivity.java
+++ app/src/main/java/mw/layoutexample/androidlayoutexample/MainActivity.java
@@ -1,7 +1,10 @@
 package mw.layoutexample.androidlayoutexample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void send(View view){
+        EditText messageEditText = (EditText)findViewById(R.id.messageTextView);
+        EditText toEditText = (EditText)findViewById(R.id.toTextView);
+        EditText subjectEditText = (EditText)findViewById(R.id.subjectTextView);
+        final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle(R.string.alertDialogTitle);
+        alertDialogBuilder.setMessage(String.format(getResources().getString(R.string.alertDialogMessage), messageEditText.getText(), subjectEditText.getText(), toEditText.getText()));
+        alertDialogBuilder.setNeutralButton(android.R.string.ok,null);
+        alertDialogBuilder.show();
+    }
+
 }
