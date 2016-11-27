4dcf798c51f4dacb16300683724f5b51679832ef
mateuszwosik <mateusz.wosik@gmail.com>
2016-11-27 17:08:29 +0100

    done

--- app/src/main/java/mw/layoutexample/androidlayoutexample/MainActivity.java
+++ app/src/main/java/mw/layoutexample/androidlayoutexample/MainActivity.java
@@ -1,7 +1,13 @@
 package mw.layoutexample.androidlayoutexample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.albums));
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(),"Album selected: " + ((TextView)view).getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
